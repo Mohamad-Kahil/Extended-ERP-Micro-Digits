4b7a86732fcfb02cbfab1c76bc22af69879f34d0
Mohamad-Kahil <mkahil@diegypt.com>
2025-03-09 13:44:30 +0000

    u

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -162,26 +162,7 @@ const Sidebar = ({ collapsed = false }: SidebarProps) => {
       ),
     },
     {
-      title: "Product Management",
-      href: "/product-management",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="14"
-          height="14"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M20.38 3.46 16 2a4 4 0 0 1-8 0L3.62 3.46a2 2 0 0 0-1.34 2.23l.58 3.47a1 1 0 0 0 .99.84H6v10c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V10h2.15a1 1 0 0 0 .99-.84l.58-3.47a2 2 0 0 0-1.34-2.23z" />
-        </svg>
-      ),
-    },
-    {
-      title: "HR",
+      title: "Human Resource",
       href: "/hr",
       icon: (
         <svg
@@ -203,7 +184,7 @@ const Sidebar = ({ collapsed = false }: SidebarProps) => {
       ),
     },
     {
-      title: "Administration",
+      title: "System Administration",
       href: "/administration",
       icon: (
         <svg
@@ -222,6 +203,26 @@ const Sidebar = ({ collapsed = false }: SidebarProps) => {
         </svg>
       ),
     },
+    {
+      title: "Product Management",
+      href: "/product-management",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M20.38 3.46 16 2a4 4 0 0 1-8 0L3.62 3.46a2 2 0 0 0-1.34 2.23l.58 3.47a1 1 0 0 0 .99.84H6v10c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V10h2.15a1 1 0 0 0 .99-.84l.58-3.47a2 2 0 0 0-1.34-2.23z" />
+        </svg>
+      ),
+    },
+
     {
       title: "Legal",
       href: "/legal",
