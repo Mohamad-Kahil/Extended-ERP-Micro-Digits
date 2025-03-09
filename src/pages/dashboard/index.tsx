38103af0edc7df7a032c29008965677cde7de9c2
Mohamad-Kahil <mkahil@diegypt.com>
2025-03-09 12:53:51 +0000

    u

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -327,7 +327,7 @@ const Dashboard = () => {
           <path d="M9 17h5" />
         </svg>
       ),
-      color: "bg-green-500",
+      color: "bg-pink-500",
       position: { x: 0, y: 3 },
       href: "/crm",
     },
@@ -352,7 +352,7 @@ const Dashboard = () => {
           <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
         </svg>
       ),
-      color: "bg-pink-500",
+      color: "bg-green-500",
       position: { x: 1, y: 3 },
       href: "/ecommerce",
     },
@@ -449,7 +449,7 @@ const Dashboard = () => {
           <path d="M12 3v6" />
         </svg>
       ),
-      color: "bg-indigo-500",
+      color: "bg-blue-500",
       position: { x: 1, y: 4 },
       href: "/logistics",
     },
@@ -472,7 +472,7 @@ const Dashboard = () => {
           <path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z" />
         </svg>
       ),
-      color: "bg-orange-600",
+      color: "bg-red-600",
       position: { x: 2, y: 4 },
       href: "/maintenance",
     },
@@ -498,7 +498,7 @@ const Dashboard = () => {
           <rect width="7" height="5" x="3" y="16" rx="1" />
         </svg>
       ),
-      color: "bg-blue-500",
+      color: "bg-indigo-500",
       position: { x: 0, y: 0 },
       href: "/dashboard",
     },
@@ -524,7 +524,7 @@ const Dashboard = () => {
           <rect width="7" height="5" x="3" y="16" rx="1" />
         </svg>
       ),
-      color: "bg-blue-500",
+      color: "bg-yellow-500",
       position: { x: 0, y: 0 },
       href: "/dashboard",
     },
